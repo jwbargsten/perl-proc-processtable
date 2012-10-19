9af0969e23a32c29ab650d27b5f91408f04ae0b8
Jonathan Swartz <swartz@pobox.com>
2012-10-19 06:32:20 -0700

    mod

--- clean.pl
+++ clean.pl
@@ -3,5 +3,5 @@ use IPC::System::Simple qw(run);
 use warnings;
 use strict;
 
-run("make distclean");
-run("rm -fR Proc-ProcessTable-* Proc-ProcessTable-*.tar.gz");
+system("make distclean");
+run("rm -fR Proc-ProcessTable-* Proc-ProcessTable-*.tar.gz MANIFEST.bak");
