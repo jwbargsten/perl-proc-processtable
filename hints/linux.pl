fa3aaa1bfa830b3a6e7af9869b4a97d3f866980b
Joachim Bargsten <jw@bargsten.org>
2012-10-21 18:13:40 +0200

    fixed bug #46861,#58236 work on perl w/o threads
    https://rt.cpan.org/Public/Bug/Display.html?id=46861
    https://rt.cpan.org/Public/Bug/Display.html?id=58236
    
    If libpthread is not used in the perl configuration, Proc::ProcessTable
    will not work. Applied the changes suggested in RT.

--- hints/linux.pl
+++ hints/linux.pl
@@ -1,1 +1,12 @@
+use Config;
+
 symlink "os/Linux.c", "OS.c" || die "Could not link os/Linux.c to os/OS.c\n";
+
+# We might have a non-threading perl, which doesn't add this
+# necessary link option.
+my $thread_lib = "-lpthread";
+
+if( $Config{libs} !~ /(?:^|\s)$thread_lib(?:\s|$)/ ) {
+  $self->{LIBS} ||= [];
+  push @{ $self->{LIBS} }, $thread_lib;
+}
