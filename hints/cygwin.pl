a9954dfa24275532d5ee362fcba5138df357c701
Joachim Bargsten <jw@bargsten.org>
2021-09-27 00:23:29 +0200

    fixed syntax error in cygwin.pl

--- hints/cygwin.pl
+++ hints/cygwin.pl
@@ -1,7 +1,8 @@
-my @files = [ "Cygwin.c", "obstack.c", "obstack.h", "obstack_printf.c" ]
+my @files = ( "Cygwin.c", "obstack.c", "obstack.h", "obstack_printf.c" );
 
 for my $f (@files) {
   symlink "os/$f", $f || die "Could not link os/$f to $f\n";
 }
 
 delete $self->{LIBS};
+$self->{OBJECT} .= ' obstack.o obstack_printf.o';
